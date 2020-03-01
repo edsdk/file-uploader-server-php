97e6781e01a55a450469eeb0dfa51da8370e51f7
dmitriy-komarov <dmitriy-komarov@3627127.no-reply.drupal.org>
2020-03-01 17:15:05 +0300

    Path fix

--- src/lib/action/ActionUploadCommit.php
+++ src/lib/action/ActionUploadCommit.php
@@ -51,6 +51,9 @@ class ActionUploadCommit extends AActionUploadId {
 		$req->autoRename = $this->validateBoolean($req->autoRename, false);
         $req->dir = $this->validateString($req->dir, "");
 
+        if (strpos($req->dir) !== 0)
+            $req->dir="/".$req->dir;
+
 		if (UtilsPHP::normalizeNoEndSeparator($req->dir) === null) {
             throw new MessageException(Message::createMessage(Message::DIR_DOES_NOT_EXIST, $req->dir));
         }
